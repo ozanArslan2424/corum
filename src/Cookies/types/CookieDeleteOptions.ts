b57704b02e7a75a2663b9e76d205244bff10c02d
ozanArslan2424 <136094471+ozanArslan2424@users.noreply.github.com>
2026-04-02 16:19:25 +0300

    knip

--- src/Cookies/types/CookieDeleteOptions.ts
+++
@@ -1,5 +0,0 @@
-export type CookieDeleteOptions = {
-	name: string;
-	domain?: string | null;
-	path?: string;
-};
--- src/utils/isRegexMatch.ts
+++
@@ -1,3 +0,0 @@
-export function isRegexMatch(source: string, pattern: RegExp): boolean {
-	return pattern.test(source);
-}
